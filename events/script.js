35b90d25e6f9e317fdcb26e6b35654ceac38e512
creative_nh <s10245614@connect.np.edu.sg>
2022-12-03 20:28:09 +0800

    - Added fonts to all pages.

--- events/script.js
+++ events/script.js
@@ -1,13 +1,13 @@
 
 var a = document.querySelectorAll(".event1")
 for (var i=0; i<a.length; i++){
-  a[i].onclick = function() {togglepopup("popup")}
+  a[i].onclick = function() {togglepopup("popup1")}
 }
 var b = document.querySelectorAll(".event2")
 for (var i=0; i<b.length; i++){
   b[i].onclick = function() {togglepopup("popup2")}
 }
-document.getElementById("cross").onclick = function() {togglepopup("popup")};
+document.getElementById("cross").onclick = function() {togglepopup("popup1")};
 document.getElementById("cross2").onclick = function() {togglepopup("popup2")};
 function togglepopup(id) {
     var x = document.getElementById(id);
